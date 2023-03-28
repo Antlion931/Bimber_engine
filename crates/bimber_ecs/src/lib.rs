641ed77fab559f7fd83ba771babc2b38310b6e1b
Antlion931 <AlbertK931@protonmail.com>
2023-03-28 19:23:15 +0200

    Entity System added

--- crates/bimber_ecs/src/lib.rs
+++ crates/bimber_ecs/src/lib.rs
@@ -1,5 +1,10 @@
 //! Testing docs
 
+use std::{collections::HashMap, any::{TypeId, Any}, hash::Hash};
+use anymap::AnyMap;
+
+type ID = u64;
+
 /// Doc test
 /// # Examples
 /// ```
@@ -12,6 +17,41 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+#[derive(Debug)]
+struct EntitySystem {
+    components: HashMap<TypeId, Vec<Option<Box<dyn Any>>>>,
+    amount_of_entities: u64,
+}
+
+impl EntitySystem {
+    fn new() -> Self{
+        Self {
+            components: HashMap::new(),
+            amount_of_entities: 0,
+        }
+    }
+
+    fn add_entity(mut self) -> Self {
+        for (_, vec) in self.components.iter_mut() {
+            vec.push(None);
+        }
+        self.amount_of_entities += 1;
+
+        self
+    }
+
+    fn with<T: 'static>(mut self, component: T) -> Self {
+        let vec = self.components
+            .entry(component.type_id())
+            .or_insert((0..self.amount_of_entities).map(|_| None).collect());
+
+        *vec.last_mut().expect("with should be called after at least one add_entity") = Some(Box::new(component));
+
+        self
+    }
+    
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -20,4 +60,19 @@ mod tests {
     fn unit_test(){
         assert_eq!(add(2,2), 4);
     }
+
+    #[test]
+    fn entity_system_add() {
+        println!("{:#?}", EntitySystem::new()
+            .add_entity()
+            .with("Albert")
+            .with(12)
+            .add_entity()
+            .with(13)
+            .add_entity()
+            .with("Dawid")
+            .with(3.14));
+
+    }
+
 }
