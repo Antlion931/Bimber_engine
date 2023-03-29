00f476208075be0a35fa442a6e7a3e864749cf0f
Antlion931 <AlbertK931@protonmail.com>
2023-03-29 23:42:41 +0200

    Added basic query methods

--- crates/bimber_ecs/src/entity_system/mod.rs
+++ crates/bimber_ecs/src/entity_system/mod.rs
@@ -1,6 +1,6 @@
 //!  Entity System for bimber ecs, manges thing like adding, deleting and quering entities.
 
-use std::{collections::HashMap, any::{TypeId, Any}};
+use std::{collections::HashMap, any::{TypeId, Any}, iter};
 
 /// EntitySystem is core struct, it handles every thing related to entites, for now usage looks
 /// like this:
@@ -57,12 +57,122 @@ impl EntitySystem {
 
         self
     }
+
+    /// Basic quering, only on one component
+    pub fn query_with_one<'a, T: 'static>(&'a self) -> Box<dyn Iterator<Item = &T> + 'a> {
+        if let Some(vec) = self.components.get(&TypeId::of::<T>()) {
+            Box::new(vec.iter().filter_map(|x| x.as_ref().map(|x| x.downcast_ref::<T>().expect("Chosen vec should only contain values of T type"))))
+        } else {
+            Box::new(iter::empty())
+        }
+    }
+
+    /// Basic mutable quering, only on one component
+    pub fn mut_query_with_one<'a, T: 'static>(&'a mut self) -> Box<dyn Iterator<Item = &mut T> + 'a> {
+        if let Some(vec) = self.components.get_mut(&TypeId::of::<T>()) {
+            Box::new(vec.iter_mut().filter_map(|x| x.as_mut().map(|x| x.downcast_mut::<T>().expect("Chosen vec should only contain values of T type"))))
+        } else {
+            Box::new(iter::empty())
+        }
+    }
     
+    /// Basic quering, only on one component
+    pub fn query_with_two<'a, T: 'static, Y: 'static>(&'a self) -> Box<dyn Iterator<Item = (&T, &Y)> + 'a> {
+        if let (Some(vec_t), Some(vec_y)) = (self.components.get(&TypeId::of::<T>()), self.components.get(&TypeId::of::<Y>())) {
+            Box::new(
+                vec_t
+                     .iter()
+                     .zip(vec_y.iter())
+                     .filter_map(|(t, y)| {
+                         if let (Some(t), Some(y)) = (t.as_ref(), y.as_ref()) {
+                             Some(
+                                    (t.downcast_ref::<T>().expect("Chosen vec should only contain values of T type"), 
+                                     y.downcast_ref::<Y>().expect("Chosen vec should only contain values of Y type"))
+                                )
+                         } else {
+                             None
+                         }
+                     }))
+        } else {
+            Box::new(iter::empty())
+        }
+    }
+
+
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn entity_system_query_with_two_works_in_complex_example() {
+        let es = EntitySystem::new()
+            .add_entity()
+            .with(12)
+            .with("test")
+            .add_entity()
+            .with(3.14)
+            .with("Alfred")
+            .add_entity()
+            .with(15)
+            .with("Hallo");
+
+        assert_eq!(es.query_with_two::<&str, i32>().count(), 2);
+    }
+
+
+    #[test]
+    fn entity_system_query_over_two_works_in_simple_example() {
+        let es = EntitySystem::new()
+            .add_entity()
+            .with(12)
+            .with("test");
+
+        assert_eq!(es.query_with_two::<i32, &str>().count(), 1);
+    }
+
+    #[test]
+    fn entity_system_mut_query_is_able_to_mutate_value() {
+        let mut es = EntitySystem::new()
+            .add_entity()
+            .with(12)
+            .with("test")
+            .add_entity()
+            .with(3.14)
+            .with("Alfred")
+            .add_entity()
+            .with(15);
+
+        es.mut_query_with_one::<i32>().for_each(|x| *x *= 2);
+
+        assert_eq!(es.query_with_one::<i32>().sum::<i32>(), 12*2  + 15*2);
+    }
+
+    #[test]
+    fn entity_system_query_with_one_works_in_simple_example() {
+        let es = EntitySystem::new()
+            .add_entity()
+            .with(12);
+
+        assert_eq!(es.query_with_one::<i32>().count(), 1);
+    }
+
+    #[test]
+    fn entity_system_query_with_one_works_in_complex_example() {
+        let es = EntitySystem::new()
+            .add_entity()
+            .with(12)
+            .with("test")
+            .add_entity()
+            .with(3.14)
+            .with("Alfred")
+            .add_entity()
+            .with(15);
+
+        assert_eq!(es.query_with_one::<i32>().count(), 2);
+    }
+    
 
     #[test]
     fn new_entity_system_is_empty() {
